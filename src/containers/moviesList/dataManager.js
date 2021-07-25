bbaaabb8fd95804f08cd6af6f231fe47a877cb73
inderpreetkaur2000 <52965575+inderpreetkaur2000@users.noreply.github.com>
2021-07-25 17:26:15 +0530

    Add files via upload

---
+++ src/containers/moviesList/dataManager.js
@@ -0,0 +1,7 @@
+  
+import { getService } from "../../services/movies";
+
+export const HandleGetMovies = async () => {
+    const result = await getService(); 
+    return result;
+}
---
+++ src/services/movies.js
@@ -0,0 +1,7 @@
+  
+import axios from "axios"
+
+export const getService = async () => {
+    const response = await axios.get("https://namanmov.free.beeceptor.com/movies");
+    return response.data;
+}
